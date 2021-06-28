5a55caa44963aff460408cfb95d034ff5d30aabe
jas-williams <73598288+jas-williams@users.noreply.github.com>
2021-06-28 20:08:41 +0100

    Create datacake-decoder.js

---
+++ CubeCell AB01/datacake-decoder.js
@@ -0,0 +1,40 @@
+function Decoder(bytes, port) {
+  tempC = bytesToFloat(bytes.slice(0, 4));
+
+  humidity = bytesToFloat(bytes.slice(4, 8));
+  
+  datacakeFields = [
+  
+	{
+	field: "TEMPERATURE",
+	value: tempC
+	},
+	{
+	field: "HUMIDITY",
+	value: humidity,
+	},
+	{
+	field: "BATTERY_VOLTAGE",
+	value: (((bytes[8])*0.2)/10).toFixed(2)
+	},
+	{
+	field: "BATTERY_LEVEL",
+	value: (bytes[9])
+	},
+	{
+	field: "DOOR_OPEN",
+	value: (((bytes[10]) !== 0) ? true : false)
+	},
+  ];
+  return datacakeFields;
+}
+
+  function bytesToFloat(bytes) {
+    //LSB Format (least significant byte first).
+    var bits = bytes[3]<<24 | bytes[2]<<16 | bytes[1]<<8 | bytes[0];
+    var sign = (bits>>>31 === 0) ? 1.0 : -1.0;
+    var e = bits>>>23 & 0xff;
+    var m = (e === 0) ? (bits & 0x7fffff)<<1 : (bits & 0x7fffff) | 0x800000;
+    var f = sign * m * Math.pow(2, e - 150);
+    return f;
+  }
