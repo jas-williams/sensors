390e1781498bc532e36f68ea61d0846ea1be420f
jas-williams <73598288+jas-williams@users.noreply.github.com>
2022-01-07 17:50:08 +0000

    Add files via upload

---
+++ CubeCell AB01/CubeCell-Temp.ino
@@ -0,0 +1,235 @@
+#include "LoRaWan_APP.h"
+#include "Arduino.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into GPIO1 on the CubeCell
+#define ONE_WIRE_BUS GPIO1
+
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
+
+/*
+ * set LoraWan_RGB to Active,the RGB active in loraWan
+ * RGB red means sending;
+ * RGB purple means joined done;
+ * RGB blue means RxWindow1;
+ * RGB yellow means RxWindow2;
+ * RGB green means received done;
+ */
+
+/* OTAA para*/
+uint8_t devEui[] = { 0x9B, 0x21, 0xA1, 0x95, 0x21, 0x3C, 0x34, 0xC3 };
+uint8_t appEui[] = { 0xE5, 0x32, 0x4F, 0xAE, 0xC8, 0xDC, 0xE7, 0x4E };
+uint8_t appKey[] = { 0xBB, 0xEF, 0x77, 0x09, 0xA9, 0x27, 0x9A, 0x54, 0x87, 0x17, 0x5F, 0x76, 0x3D, 0x92, 0xAB, 0xD2 };
+
+/* ABP para*/
+uint8_t nwkSKey[] = { 0x15, 0xb1, 0xd0, 0xef, 0xa4, 0x63, 0xdf, 0xbe, 0x3d, 0x11, 0x18, 0x1e, 0x1e, 0xc7, 0xda,0x85 };
+uint8_t appSKey[] = { 0xd7, 0x2c, 0x78, 0x75, 0x8c, 0xdc, 0xca, 0xbf, 0x55, 0xee, 0x4a, 0x77, 0x8d, 0x16, 0xef,0x67 };
+uint32_t devAddr =  ( uint32_t )0x007e6ae1;
+
+/*LoraWan channelsmask, set for USA Band 2 (Helium)*/ 
+uint16_t userChannelsMask[6]={ 0xFF00,0x0000,0x0000,0x0000,0x0000,0x0000 };
+
+/*LoraWan region, select in arduino IDE tools*/
+LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
+
+/*LoraWan Class, Class A and Class C are supported*/
+DeviceClass_t  loraWanClass = LORAWAN_CLASS;
+
+/*the application data transmission duty cycle.  value in [ms].*/
+uint32_t appTxDutyCycle = 1200000; //update every 20 miniutes
+
+
+/*OTAA or ABP*/
+bool overTheAirActivation = LORAWAN_NETMODE;
+
+/*ADR enable*/
+bool loraWanAdr = LORAWAN_ADR;
+
+/* set LORAWAN_Net_Reserve ON, the node could save the network info to flash, when node reset not need to join again */
+bool keepNet = LORAWAN_NET_RESERVE;
+
+/* Indicates if the node is sending confirmed or unconfirmed messages */
+bool isTxConfirmed = LORAWAN_UPLINKMODE;
+
+/* Application port */
+uint8_t appPort = 2;
+/* Temp Variable */
+float celsius = 0 ;
+/*!
+* Number of trials to transmit the frame, if the LoRaMAC layer did not
+* receive an acknowledgment. The MAC performs a datarate adaptation,
+* according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
+* to the following table:
+*
+* Transmission nb | Data Rate
+* ----------------|-----------
+* 1 (first)       | DR
+* 2               | DR
+* 3               | max(DR-1,0)
+* 4               | max(DR-1,0)
+* 5               | max(DR-2,0)
+* 6               | max(DR-2,0)
+* 7               | max(DR-3,0)
+* 8               | max(DR-3,0)
+*
+* Note, that if NbTrials is set to 1 or 2, the MAC will not decrease
+* the datarate, in case the LoRaMAC layer did not receive an acknowledgment
+*/
+uint8_t confirmedNbTrials = 4;
+
+/* Prepares the payload of the frame */
+static void prepareTxFrame( uint8_t port )
+{
+	/*appData size is LORAWAN_APP_DATA_MAX_SIZE which is defined in "commissioning.h".
+	*appDataSize max value is LORAWAN_APP_DATA_MAX_SIZE.
+	*if enabled AT, don't modify LORAWAN_APP_DATA_MAX_SIZE, it may cause system hanging or failure.
+	*if disabled AT, LORAWAN_APP_DATA_MAX_SIZE can be modified, the max value is reference to lorawan region and SF.
+	*for example, if use REGION_CN470, 
+	*the max value for different DR can be found in MaxPayloadOfDatarateCN470 refer to DataratesCN470 and BandwidthsCN470 in "RegionCN470.h".
+	*/
+    //Wakeup Temp Sensor
+    // Vext ON
+    Serial.println("Power up Dallas");
+    pinMode(Vext, OUTPUT);
+    digitalWrite(Vext, LOW);
+    delay(1000);
+    sensors.begin();  
+    // call sensors.requestTemperatures() to issue a global temperature
+    // request to all devices on the bus
+    Serial.print("Requesting temperatures...");
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    Serial.println("DONE");
+    // After we got the temperatures, we can print them here.
+    // We use the function ByIndex, and as an example get the temperature from the first sensor only.
+    celsius = sensors.getTempCByIndex(0);
+    Serial.print("Temperature for the device 1 (index 0) is: ");
+    Serial.println(celsius);
+    delay(500);
+    
+    // Vext OFF
+    digitalWrite(Vext, HIGH);
+    
+    //Get Battery Voltage
+    uint16_t batteryVoltage = getBatteryVoltage();
+    Serial.print("Battery Voltage ");
+    Serial.print(batteryVoltage/1000);
+    Serial.println(" Volts");    
+    
+    //get Battery Level 1-254 Returned by BoardGetBatteryLevel
+    float batteryLevel = BoardGetBatteryLevel();
+    //Convert to %
+    batteryLevel = (batteryLevel/254)*100;
+
+    Serial.print("Battery Level ");
+    Serial.print(batteryLevel);
+    Serial.println(" %");    
+    
+    //Build Payload
+    unsigned char *puc;
+    if (celsius > -127) {
+      puc = (unsigned char *)(&celsius);
+      appDataSize = 6;
+      appData[0] = puc[0];
+      appData[1] = puc[1];
+      appData[2] = puc[2];
+      appData[3] = puc[3];
+      appData[4] = (uint8_t)((batteryVoltage/20) & 0xFF);
+      appData[5] = (uint8_t)batteryLevel;
+
+    }
+    else {
+      Serial.println("Invalid Temperature Not sending");
+      }
+}
+
+void downLinkDataHandle(McpsIndication_t *mcpsIndication)
+{
+  Serial.printf("+REV DATA:%s,RXSIZE %d,PORT %d\r\n",mcpsIndication->RxSlot?"RXWIN2":"RXWIN1",mcpsIndication->BufferSize,mcpsIndication->Port);
+  Serial.print("+REV DATA:");
+  for(uint8_t i=0;i<mcpsIndication->BufferSize;i++)
+  {
+    Serial.printf("%02X",mcpsIndication->Buffer[i]);
+  }
+  Serial.println();
+  uint32_t color=mcpsIndication->Buffer[0]<<16|mcpsIndication->Buffer[1]<<8|mcpsIndication->Buffer[2];
+#if(LoraWan_RGB==1)
+  turnOnRGB(color,5000);
+  turnOffRGB();
+#endif
+}
+
+
+void setup() {
+	//Vext ON
+  pinMode(Vext,OUTPUT);
+  digitalWrite(Vext,LOW);
+  delay(1000);
+  boardInitMcu();
+	Serial.begin(115200);
+  // Start up the dallas library
+  sensors.begin();
+ 
+ 
+#if(AT_SUPPORT)
+	enableAt();
+#endif
+	deviceState = DEVICE_STATE_INIT;
+	LoRaWAN.ifskipjoin();
+}
+
+void loop()
+{
+	switch( deviceState )
+	{
+		case DEVICE_STATE_INIT:
+		{
+#if(AT_SUPPORT)
+			getDevParam();
+#endif
+			printDevParam();
+			LoRaWAN.init(loraWanClass,loraWanRegion);
+			deviceState = DEVICE_STATE_JOIN;
+			break;
+		}
+		case DEVICE_STATE_JOIN:
+		{
+			LoRaWAN.join();
+			break;
+		}
+		case DEVICE_STATE_SEND:
+		{
+		
+      prepareTxFrame( appPort );
+				
+			
+			LoRaWAN.send();
+			deviceState = DEVICE_STATE_CYCLE;
+			break;
+		}
+		case DEVICE_STATE_CYCLE:
+		{
+			// Schedule next packet transmission
+			txDutyCycleTime = appTxDutyCycle + randr( 0, APP_TX_DUTYCYCLE_RND );
+			LoRaWAN.cycle(txDutyCycleTime);
+			deviceState = DEVICE_STATE_SLEEP;
+			break;
+		}
+		case DEVICE_STATE_SLEEP:
+		{
+			LoRaWAN.sleep();
+			break;
+		}
+		default:
+		{
+			deviceState = DEVICE_STATE_INIT;
+			break;
+		}
+	}
+}
